692eced5e147d10c5cf2dbf47a87c9dd6224a58b
Sergey Tarasenko <krasina15@gmail.com>
2017-01-30 15:54:04 +0500

    move to github

---
+++ NSBG/BarcodeGenerator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Drawing;
+using System.Drawing.Imaging;
+using Nancy;
+using BarcodeLib;
+
+namespace NancyServer
+
+{
+	public class GenerateCode : NancyModule
+	{
+		public Image GenerateIt(string bdata, TYPE btype, int bwight, int bheight)
+		{
+			BarcodeLib.Barcode imgcode = new BarcodeLib.Barcode(bdata, btype);
+			imgcode.Width = bwight;
+			imgcode.Height = bheight;
+			Image img = imgcode.Encode(btype, bdata);
+			return img;
+		}
+		public GenerateCode()
+		{
+			Get["/gen"] = x =>
+			{
+				var bdata = this.Request.Query["data"];
+				var btype = this.Request.Query["type"];
+				var bwight = this.Request.Query["wight"];
+				var bheight = this.Request.Query["height"];
+				var parsedType = (TYPE) Enum.Parse(typeof(TYPE), btype);
+                Image b = GenerateIt(bdata, parsedType, bwight, bheight);
+				var ms = new MemoryStream();
+				b.Save(ms, ImageFormat.Png);
+				ms.Position = 0;
+				string ContentType = "image/png";
+				return Response.FromStream(ms, ContentType);
+			};
+		}
+	}
+}
---
+++ NSBG/Program.cs
@@ -0,0 +1,40 @@
+﻿using Nancy.Hosting.Self;
+using System;
+using Mono.Unix;
+
+namespace NSBG
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var uri = "http://localhost:8888";
+			Console.WriteLine("Starting Nancy on " + uri);
+
+			// initialize an instance of NancyHost
+			var host = new NancyHost(new Uri(uri));
+			host.Start();  // start hosting
+
+			// check if we're running on mono
+			if (Type.GetType("Mono.Runtime") != null)
+			{
+				// on mono, processes will usually run as daemons - this allows you to listen
+				// for termination signals (ctrl+c, shutdown, etc) and finalize correctly
+
+				UnixSignal.WaitAny(new[] {
+										new UnixSignal(Mono.Unix.Native.Signum.SIGINT),
+										new UnixSignal(Mono.Unix.Native.Signum.SIGTERM),
+										new UnixSignal(Mono.Unix.Native.Signum.SIGQUIT),
+										new UnixSignal(Mono.Unix.Native.Signum.SIGHUP)
+								});
+			}
+			else
+			{
+				Console.ReadLine();
+			}
+
+			Console.WriteLine("Stopping Nancy");
+			host.Stop();  // stop hosting
+		}
+	}
+}
